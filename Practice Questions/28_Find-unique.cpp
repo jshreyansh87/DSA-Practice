45fc0401c29eb6337bd84bff3f3adee312603de7
Shreyansh Jain <jshreyansh87@gmail.com>
2023-03-18 13:53:42 +0530

    solved find unique

---
+++ Practice Questions/28_Find-unique.cpp
@@ -0,0 +1,16 @@
+// https://www.codingninjas.com/codestudio/problems/find-unique_625159
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int findUnique(int *arr, int size)
+{
+    int ans = 0;
+
+    for (int i = 0; i < size; i++)
+    {
+        ans = ans ^ arr[i];
+    }
+
+    return ans;
+}
